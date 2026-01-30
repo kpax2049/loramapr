3ece6792887f223f67a7ce83c58a34e8c2813816
Kirill <kpax2049@gmail.com>
2026-01-30 21:44:42 +0100

    Add Leaflet map base with asset fixes. Include Leaflet CSS and full-screen MapView.

---
+++ frontend/src/components/MapView.tsx
@@ -0,0 +1,45 @@
+import { useEffect } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import L from 'leaflet';
+
+const DEFAULT_CENTER: [number, number] = [37.7749, -122.4194];
+const DEFAULT_ZOOM = 12;
+
+const markerIconUrl = new URL('leaflet/dist/images/marker-icon.png', import.meta.url).toString();
+const markerRetinaUrl = new URL(
+  'leaflet/dist/images/marker-icon-2x.png',
+  import.meta.url,
+).toString();
+const markerShadowUrl = new URL('leaflet/dist/images/marker-shadow.png', import.meta.url).toString();
+
+type MapViewProps = {
+  center?: [number, number];
+  zoom?: number;
+};
+
+function configureLeafletIcons() {
+  L.Icon.Default.mergeOptions({
+    iconUrl: markerIconUrl,
+    iconRetinaUrl: markerRetinaUrl,
+    shadowUrl: markerShadowUrl
+  });
+}
+
+export default function MapView({ center = DEFAULT_CENTER, zoom = DEFAULT_ZOOM }: MapViewProps) {
+  useEffect(() => {
+    configureLeafletIcons();
+  }, []);
+
+  return (
+    <MapContainer
+      center={center}
+      zoom={zoom}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  );
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClientProvider } from '@tanstack/react-query';
+import 'leaflet/dist/leaflet.css';
 
 import App from './App';
 import './index.css';
