4059689a4b530a86a2ca0b86b5736238bcf7ab71
Kirill <kpax2049@gmail.com>
2026-01-30 18:27:02 +0100

    Add frontend Vite React app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1>LoRaMapr</h1>
+        <p>Vite + React + TypeScript</p>
+      </header>
+      <main className="app__content">
+        <p>
+          Start by editing <code>src/App.tsx</code>.
+        </p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
